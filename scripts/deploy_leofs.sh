3e2acf7c55e57114f85f926adfd8e8901a165961
mocchira <nekotaroh@gmail.com>
2016-07-15 13:57:15 +0900

    Fix for integration-test

--- scripts/deploy_leofs.sh
+++ scripts/deploy_leofs.sh
@@ -2,7 +2,6 @@
 set -e
 
 cp leofs_concourse/ansible/deploy_leofs.yml leofs_ansible/
-cp leofs_concourse/ansible/roles/leo_manager_0/defaults/main.yml leofs_ansible/roles/leo_manager_0/defaults/
 
 echo $ANSIBLE_KEY > ansible_key
 sed -i -e 's/ /\n/g; s/BEGIN\nRSA\nPRIVATE\nKEY/BEGIN RSA PRIVATE KEY/g; s/END\nRSA\nPRIVATE\nKEY/END RSA PRIVATE KEY/g' ansible_key
@@ -14,7 +13,3 @@ ansible-playbook -i $ANSIBLE_INVENTORY -b -u leofs --private-key=ansible_key leo
 
 ./leofs-adm status
 ./leofs-adm add-endpoint $LEOFS_GW_HOST
-sleep 5
-./leofs-adm add-endpoint $LEOFS_GW_HOST
-
-./leofs-adm add-bucket test 05236
